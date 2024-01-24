bb3d1b167ed55848896961984c12b9900b1610da
EricBoix <eric.boix@insa-lyon.fr>
2024-01-24 19:04:44 +0100

    The terrain opacity is reduced when we get close to cave.

--- src/index.js
+++ src/index.js
@@ -41,9 +41,9 @@ loadMultipleJSON([
         camera: {
           default: {
             position: {
-              x: 1841912.9,
-              y: 5175321.1,
-              z: 316.0,
+              x: 1841881.83,
+              y: 5175148.70,
+              z: 300.00,
             },
           },
         },
@@ -154,4 +154,93 @@ loadMultipleJSON([
     window.addEventListener('keydown', (event) => {
       if (event.key == 'p') console.log(app);
     });
+
+    // Because this PointCloudVisualizer based application is dedicated/specific
+    // to visualizing 3DTileset of tunnels and caves the geometric structures
+    // of interest will (most often) be subterrenean. This pauses the difficulty
+    // of having to deal with either viewing the terrain (and hidding the
+    // subterranean structures of interest) or viewing the subterranean 
+    // structures (while visually setting aside the terrain).
+    // The empirical balance (between terrain and subterranean structure) we 
+    // use uses a distance criteria and goes:
+    // * when we get close "enough" to the subterranean strucutre we want to 
+    //   facilitate the view of the tileset by automatically making the natural 
+    //   terrain transparent. 
+    // * when getting away far "enough" from the tileset when want to privilege
+    //   the viewing of the terrain and thus to restore the inital value of 
+    //   the opacity of the terrain.
+
+    function isCameraInsideZoneOfInterest(app) {
+      // Consider the first point cloud managed by the PointCloudVisualizer
+      // and compute the center of its bounding box.
+      if (typeof app.pointCloudLayers[0] === 'undefined') {
+        console.log("Unfound point cloud.");
+        return false;
+      }
+      if (typeof app.pointCloudLayers[0].root === 'undefined') {
+        console.log("Unfound rootTile.");
+        return false;
+      }
+      const rootTile = app.pointCloudLayers[0].root;
+      const rootTilePosition = rootTile.position;
+      const rootTileBox = rootTile.boundingVolume.box;
+      var boxMin = rootTileBox.min.clone();
+      var boxMax = rootTileBox.max.clone();
+      var boxCenter = boxMin.add(boxMax).multiplyScalar(1 / 2);
+      boxCenter = boxCenter.add(rootTilePosition);
+
+      // Define some notion of the size/dimensions of the bounding box of the 
+      // tileset:
+      const boxDiagonal = boxMax.sub(rootTileBox.min).length();
+
+      // When we are close enough (using some empirical criteria) when change
+      // the opacity of the terrain layer
+      const cameraPosition = app.orbitControls.object.position;
+      const closeEnough = cameraPosition.distanceTo(boxCenter) - boxDiagonal;
+
+      if (closeEnough < 0.) {
+        return true;
+      }
+      return false;
+    }
+
+    // Within the context of this PointCloudVisualizer based app, the terrain
+    // is represented by PointCloudVisualizer::itownsView.tileLayer. Hence the
+    // name of the variable whose purpose is to store the value of the layer
+    // opacity on entry (of the zone of interest) in order to restore it on 
+    // exit (from the zone of interest):
+    var planarViewOpacityOnEntry = 1.0; // In theory, it could be any value.
+    var outsideOfZoneOfIntererst = isCameraInsideZoneOfInterest(app);
+
+    app.orbitControls.addEventListener('change', (event) => {
+
+      if (isCameraInsideZoneOfInterest(app)) {
+        if (outsideOfZoneOfIntererst) {
+          // We were outside of the zone of interest and we are entering it.
+          // Store the on-entry terrain opacity and make the terrain almost
+          // transparent.
+          outsideOfZoneOfIntererst = false;
+          planarViewOpacityOnEntry = app.itownsView.tileLayer.opacity;
+          app.itownsView.tileLayer.opacity = 0.2;
+          return;
+        }
+      } else {
+        // The camera is outside the zone of interest
+        if (!outsideOfZoneOfIntererst) {
+          // We were inside the zone of interest and we are exiting from it.
+          // Restore the initial value of the opacity.
+          outsideOfZoneOfIntererst = true;
+          app.itownsView.tileLayer.opacity = planarViewOpacityOnEntry;
+          planarViewOpacityOnEntry = -1.0;   // On debug purposes
+          return;
+        }
+      }
+      // There was no contradiction between where we thought we were (be it
+      // outside of inside) and were we are. We thus didn't cross the ZOI
+      // border and hence the opacity remains unchanged.
+
+      if (event.key == 'p') console.log(app)
+    });
+
+
   });
