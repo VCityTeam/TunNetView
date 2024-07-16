a78ba34392cfedb1de3da3e153ff3c4c743b0e91
EricBoix <eric.boix@insa-lyon.fr>
2024-07-16 16:49:58 +0200

    Watched URL is now configurable at run time.

---
+++ bin/backEnd.js
@@ -0,0 +1,52 @@
+/**
+ * @file Sets up an Express backend server for examples, serving static files,
+ * replacing HTML strings.
+ */
+
+const express = require('express');
+const path = require('path');
+const { stringReplace } = require('string-replace-middleware');
+
+/**
+ * The environment mode.
+ *
+ * @type {string}
+ */
+const syntheticCaveUrl = process.env.SYNTHETIC_CAVE_URL || 'https://dataset-dl.liris.cnrs.fr/synthetic-cave-and-tunnel-systems/Cave/cave_sub_4_grid_size_x_1_grid_size_y_1_point_cloud-3dtiles/tileset-translated-to-lyon-cathedral.json';
+console.log('Displaying this cave URL', syntheticCaveUrl);
+
+/**
+ * @type {number}
+ */
+const PORT = process.env.PORT || 8000;
+
+/**
+ * Express application instance.
+ *
+ * @type {object}
+ */
+const app = new express();
+
+// Apply string replacements for different values in HTML responses
+app.use(
+  stringReplace(
+    {
+      SYNTHETIC_CAVE_URL: syntheticCaveUrl,
+    },
+    {
+      contentTypeFilterRegexp: /^application\/json/,
+    }
+  )
+);
+
+// Serve static files
+app.use(express.static(path.resolve(__dirname, '../')));
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.error('Server could not start');
+    return;
+  }
+  console.log('Http server listening on port', PORT);
+});
+
