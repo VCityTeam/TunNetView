6492a9f72bc86c214d5585a32ac96f52d534bb63
Mathieu Livebardon <mathieu.livebardon@universite-lyon.fr>
2024-09-23 16:41:08 +0200

    fix(Measure):comments + remove this.itownsview

--- src/Measure.js
+++ src/Measure.js
@@ -1,7 +1,10 @@
 import { createLocalStorageDetails } from '@ud-viz/utils_browser';
+import { round, vector3ToLabel } from '@ud-viz/utils_shared';
 import {
   Group,
+  Mesh,
   MeshBasicMaterial,
+  Line,
   LineBasicMaterial,
   Object3D,
   SphereGeometry,
@@ -10,23 +13,39 @@ import {
 } from 'three';
 
 import { MAIN_LOOP_EVENTS } from 'itowns';
+import { LayerManager } from './LayerManager';
 
+/** @classdesc Measurement tool in a 3D visualization. Measure distances by clicking points in the scene and creating a path */
 export class Measure {
-  constructor(itownsView, layerManager) {
+  /**
+   * @constructor
+   * @param {PlanarView} itownsView - managing the 3D scene.
+   * @param {LayerManager} layerManager -  handling 3D tiles and intersections.
+   * @param {HTMLElement} viewerDiv - 3D viewer container.
+   */
+  constructor(itownsView, layerManager, viewerDiv) {
     this.domElement = createLocalStorageDetails('measure_details', 'Mesure');
+
+    /** @type {HTMLButtonElement} Toggling measure mode*/
     this.pathButton = null;
+    /** @type {HTMLButtonElement} Clearing the current measure path */
     this.clearMeasurePathButton = null;
+    /** @type {HTMLDivElement} */
     this.infoPointCloudClicked = null;
 
-    this.itownsView = itownsView;
+    /** @type {LayerManager} */
     this.layerManager = layerManager;
 
+    /** @type {Boolean} Flag if measure is active */
     this.modeMeasure = false;
 
+    /** @type {MeasurePath} */
     this.currentMeasurePath = null;
 
+    /** @type {Group} */
     this.group = new Group();
     this.initHtml();
+    this.initCallBack(viewerDiv, itownsView);
   }
 
   initHtml() {
@@ -41,18 +60,27 @@ export class Measure {
     this.domElement.appendChild(this.infoPointCloudClicked);
   }
 
-  initCallBack(viewerDiv) {
+  /**
+   * Initializes event callbacks for handling user interactions.
+   * Handles point selection in the scene, toggling measure mode, and clearing the current path.
+   * @param {HTMLElement} viewerDiv - 3D viewer container.
+   * @param {PlanarView} itownsView - iTowns view instance.
+   */
+  initCallBack(viewerDiv, itownsView) {
     this.clearMeasurePathButton.onclick = () => {
       if (this.currentMeasurePath) this.currentMeasurePath.dispose();
-      this.leaveMeasureMode();
+      this.leaveMeasureMode(itownsView);
     };
     this.pathButton.onclick = () => {
       this.modeMeasure = !this.modeMeasure;
-      this.update();
+      this.update(itownsView);
     };
 
     viewerDiv.addEventListener('click', (event) => {
-      const i = this.layerManager.eventTo3DTilesIntersect(event);
+      const i = this.layerManager.eventTo3DTilesIntersect(
+        event,
+        itownsView.camera.camera3D
+      );
 
       if (i) {
         this.infoPointCloudClicked.innerText =
@@ -60,24 +88,28 @@ export class Measure {
 
         // if measure mode add point to path
         if (this.modeMeasure) {
-          this.currentMeasurePath.addPoint(i.point);
+          this.currentMeasurePath.addPoint(i.point, itownsView);
         }
       }
     });
   }
 
-  initGroup() {
-    this.object3D = new Group();
-  }
-
-  leaveMeasureMode() {
+  /**
+   * Exits the measurement mode and updates the view accordingly.
+   * @param {PlanarView} itownsView - iTowns view instance.
+   */
+  leaveMeasureMode(itownsView) {
     if (this.modeMeasure) {
       this.modeMeasure = false;
-      this.updateMeasure();
+      this.update(itownsView);
     }
   }
 
-  update() {
+  /**
+   * Updates the UI and behavior based on the current measurement mode.
+   * @param {PlanarView} itownsView - iTowns view instance.
+   */
+  update(itownsView) {
     this.pathButton.innerText = this.modeMeasure
       ? 'Arreter de  mesurer'
       : 'Ajouter chemin de mesure';
@@ -85,13 +117,18 @@ export class Measure {
     if (this.modeMeasure) {
       this.domElement.classList.add('cursor_add');
       if (this.currentMeasurePath) this.currentMeasurePath.dispose();
-      this.currentMeasurePath = new MeasurePath();
+      this.currentMeasurePath = this.createNewMeasurePath(itownsView);
     } else {
       this.domElement.classList.remove('cursor_add');
     }
   }
 
-  addObjectInGroup() {
+  /**
+   * Creates a new measurement path and adds it to the group.
+   * @param {PlanarView} itownsView - iTowns view instance.
+   * @returns {MeasurePath} - New instance of the MeasurePath class.
+   */
+  createNewMeasurePath(itownsView) {
     const pointMaterial = new MeshBasicMaterial({ color: 'green' });
     const lineMaterial = new LineBasicMaterial({
       color: 0x0000ff,
@@ -99,42 +136,62 @@ export class Measure {
     });
     const newMeasurePath = new MeasurePath(
       this.group,
-      this.itownsView,
+      itownsView,
       pointMaterial,
       lineMaterial
     );
+    return newMeasurePath;
   }
 }
 
+/**
+ * @classdesc Measurement path composed of points and lines.
+ */
 class MeasurePath {
+  /**
+   * @constructor
+   * @param {Group} parentMeasureObject - Group that contains the path.
+   * @param {PlanarView} itownsView - iTowns view instance.
+   * @param {MeshBasicMaterial} pointMaterial - Material for the sphere meshes.
+   * @param {LineBasicMaterial} lineMaterial - Material for the lines.
+   */
   constructor(parentMeasureObject, itownsView, pointMaterial, lineMaterial) {
+    /**@type {Object3D} */
     this.object3D = new Object3D();
 
     parentMeasureObject.add(this.object3D);
 
-    this.itownsView = itownsView;
-
+    /**@type {MeshBasicMaterial} */
     this.pointMaterial = pointMaterial;
+    /**@type {LineBasicMaterial} */
     this.lineMaterial = lineMaterial;
 
+    /** @type {Array<Mesh>} array of spheres that representing points*/
     this.sphereMesh = [];
 
+    /** @type {Array<Label3D>} array of Label3D for distances between points*/
     this.labelDomElements = [];
 
     // record a frame requester
     itownsView.addFrameRequester(MAIN_LOOP_EVENTS.AFTER_CAMERA_UPDATE, () => {
-      this.updateTransform();
+      this.updateTransform(itownsView);
     });
 
+    /** @type {Array<Vector3>} Array of points*/
     this.points = [];
   }
 
-  update() {
-    // reset
+  /**
+   * Updates the 3D objects representing the measurement path, including spheres and lines.
+   * @param {PlanarView} itownsView - iTowns view instance.
+   */
+  update(itownsView) {
+    // clear existing objects
     while (this.object3D.children.length) {
       this.object3D.remove(this.object3D.children[0]);
     }
 
+    // add points as spheres
     for (let index = 0; index < this.points.length; index++) {
       const point = this.points[index];
       const sphere = new Mesh(new SphereGeometry(), this.pointMaterial);
@@ -144,6 +201,7 @@ class MeasurePath {
       this.object3D.add(sphere);
     }
 
+    // clear labels
     this.labelDomElements.forEach((l) => l.dispose());
     this.labelDomElements.length = 0;
 
@@ -163,6 +221,7 @@ class MeasurePath {
         min.y = Math.min(point.y, min.y);
         min.z = Math.min(point.z, min.z);
 
+        // add distance label between points
         if (cloneArray[index + 1]) {
           this.labelDomElements.push(
             new Label3D(
@@ -183,6 +242,7 @@ class MeasurePath {
         point.sub(center);
       });
 
+      // create line from points
       const line = new Line(
         new BufferGeometry().setFromPoints(cloneArray),
         this.lineMaterial
@@ -192,25 +252,34 @@ class MeasurePath {
       this.object3D.add(line);
     }
 
-    this.updateTransform();
+    this.updateTransform(itownsView);
   }
 
-  updateTransform() {
+  /**
+   * Updates the transform (scale) of spheres and labels based on camera distance.
+   * @param {PlanarView} itownsView - iTowns view instance.
+   */
+  updateTransform(itownsView) {
     this.sphereMesh.forEach((s) => {
       const scale =
-        this.itownsView.camera.camera3D.position.distanceTo(s.position) / 100;
+        itownsView.camera.camera3D.position.distanceTo(s.position) / 100;
       s.scale.set(scale, scale, scale);
     });
 
-    // update labels
-    this.labelDomElements.forEach((l) => l.update());
+    // update the position of labels on the screen
+    this.labelDomElements.forEach((l) => l.update(itownsView));
 
-    this.itownsView.notifyChange(itownsView.camera.camera3D);
+    itownsView.notifyChange(itownsView.camera.camera3D);
   }
 
-  addPoint(vector) {
+  /**
+   * Adds a new point to the measurement path.
+   * @param {Vector3} vector - position of the point.
+   * @param {PlanarView} itownsView - iTowns view instance.
+   */
+  addPoint(vector, itownsView) {
     this.points.push(vector);
-    this.update();
+    this.update(itownsView);
   }
 
   dispose() {
@@ -219,10 +288,20 @@ class MeasurePath {
   }
 }
 
+/**
+ * @classdesc 3D label that displays information
+ */
 class Label3D {
+  /**
+   * @constructor
+   * @param {Vector3} position - Position of the label.
+   * @param {string} value - text value.
+   */
   constructor(position, value) {
+    /**@type {Vector3} */
     this.position = position;
 
+    /**@type {HTMLDivElement} */
     this.domElement = document.createElement('div');
     this.domElement.classList.add('label_3D');
     this.domElement.innerText = value;
@@ -232,6 +311,10 @@ class Label3D {
     this.domElement.remove();
   }
 
+  /**
+   * Updates the position of the label on the screen based on the camera view.
+   * @param {PlanarView} itownsView - iTowns view instance.
+   */
   update(itownsView) {
     const onScreenPosition = this.position.clone();
     onScreenPosition.project(itownsView.camera.camera3D);
